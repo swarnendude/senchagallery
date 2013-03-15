a7a7a9fed6fd3a3e87db6cd4663a9a23188c5a2f
Swarnendu De <swarnendu.de@gmail.com>
2013-03-15 19:46:37 +0530

    dynamic gallery right part done properly but not left part. There are certain issues while selecting an image - which doesn't show up from 2nd time

--- app/controller/App.js
+++ app/controller/App.js
@@ -1,5 +1,6 @@
 Ext.define('Gallery.controller.App',{
 				extend : 'Ext.app.Controller',
+				numberOfImagesToShow : 10,
 				config : {
 								refs : {
 												main : 'main',
@@ -10,49 +11,93 @@ Ext.define('Gallery.controller.App',{
 								control : {
 												thumbView : {
 																itemtap : function(dataview, index, target, record){
-																				var store = Ext.getStore('Photos'),
-																				firstIndex = (index-4 < 0) ? 0 : (index - 4),
-																				lastIndex = (store.getCount() < index + 5) ? store.getCount() : (index + 5),
+																				var 	me = this,
+																				store = Ext.getStore('Photos'),
+																				firstIndex = ((index - (me.offsetIndex - 1)) < 0) ? 0 : (index - (index - (me.offsetIndex - 1))),
+																				lastIndex = 	(firstIndex < me.offsetIndex) ? (me.numberOfImagesToShow + firstIndex - 1) : ((store.getCount() < index + me.offsetIndex) ? store.getCount() : (index + me.offsetIndex)),
 																				range = store.getRange(firstIndex, lastIndex),
-																				photos = [],
-																				me = this;
+																				photos = [];
+																				
+																				console.log('firstIndex = ', firstIndex, lastIndex)
+																				
+																				me.firstIndex = firstIndex;
+																				me.lastIndex = lastIndex;
 																				
 																				for(var i=0; i<range.length; i++){
-																								photos.push({
-																												xtype : 'container',
-																												index : i + firstIndex,
-																												tpl : '<div class="largeimage"><img src="http://farm{farm}.staticflickr.com/{server}/{id}_{secret}.jpg"/></div>',
-																												data : range[i].getData(),
-																												listeners : {
-																																scope : this,
-																																initialize : function(panel){
-																																				panel.element.on('dragend', function(e, el){
-																																								setTimeout(function(){
-																																												var activePanel = me.getFullView().getActiveItem();
-																																												if(panel.id !== activePanel.id){
-																																																console.log('panel = ', panel.config.index, activePanel.config.index)
-																																																me.loadExtraImages(e.direcction);
-																																												}
-																																								}, 500);
-																																				});
-																																}
-																												}
-																								});
+																								photos.push(me.getImageContainer(i + firstIndex, range[i]));
 																				}
 																				
-																				this.getMain().add({
+																				this.getMain().push({
 																								title : record.get('title'),
 																								xtype : 'fullview'
 																				});
+																				this.getFullView().removeAll();
 																				
 																				this.getFullView().add(photos);																				
 																				this.getFullView().setActiveItem(index - firstIndex);
 																}
-												}
-								},
-								
-								loadExtraImages : function(){
+												},
 												
+												'container[name="image_container"]': {
+																initialize : function(panel){
+																				var me = this;
+																				
+																				panel.element.on('dragend', function(e, el){
+																								setTimeout(function(){
+																												var activePanel = me.getFullView().getActiveItem();
+																												if(panel.id !== activePanel.id){
+																																//																																																console.log('panel = ', panel.config.index, activePanel.config.index)
+																																me.loadExtraImages(panel.config.index < activePanel.config.index, activePanel, panel);
+																												}
+																								}, 500);
+																				});
+																}
+												}
+								}
+				},
+				
+				launch : function(){
+								this.offsetIndex = parseInt(this.numberOfImagesToShow / 2);
+				},
+				
+				getImageContainer : function(index, record){
+								if(record){
+												return {
+																xtype : 'container',
+																index : index,
+																name : 'image_container',
+																tpl : '<div class="largeimage"><img src="http://farm{farm}.staticflickr.com/{server}/{id}_{secret}.jpg"/></div>',
+																data : record.getData()
+												}
+								}
+								
+								return [];
+				},
+								
+				loadExtraImages : function(addToRight, activePanel, previousPanel){
+								var me = this,
+								activeIndex = activePanel.config.index,
+								advanceIndex = addToRight ? (activeIndex + me.offsetIndex) : (activeIndex - me.offsetIndex),
+								panels = me.getFullView().query('container[index="' + advanceIndex + '"]'),
+								store = Ext.getStore('Photos');
+								
+								if(advanceIndex >= 0 && panels.length === 0){
+												if(advanceIndex > activeIndex){
+																me.getFullView().remove(me.getFullView().query('container[index="' + me.firstIndex + '"]')[0]);
+																me.firstIndex += 1;
+																me.lastIndex += 1;
+																me.getFullView().add(me.getImageContainer(advanceIndex, store.getAt(advanceIndex)));
+												}else{
+																if(me.firstIndex >= 0){
+																				me.getFullView().remove(me.getFullView().query('container[index="' + me.lastIndex + '"]')[0]);
+																				me.firstIndex -= 1;
+																				me.lastIndex -= 1;
+																				
+																				console.log('advanceIndex = ', advanceIndex)
+																				
+																				me.getFullView().insert(0, me.getImageContainer(advanceIndex, store.getAt(advanceIndex)));
+																}
+												}
 								}
 				}
 });
